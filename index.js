ae270e8a250f01cb97d25e9b0343a8cc200d0a07
lfalvespe <lfalvespe@gmail.com>
2022-09-07 15:37:17 -0300

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function login(){
+	let email = document.querySelector('#email')
+	let senha = document.querySelector('#senha')
+	let msg = document.querySelector('.msg')
+	if (email.value != '' && senha.value != '') {
+	   msg.innerHTML = 'Login efetuado com sucesso.'
+	} else {
+		msg.innerHTML = 'Email e/ou senha inválido(s)'
+	}
+}
+function fundoEmailBranco() {
+		email.style.backgroundColor = 'white'
+}
+function fundoEmailCinza() {
+		email.style.backgroundColor = 'rgb(181, 181, 181)'
+}
+
+function fundoSenhaBranco() {
+		senha.style.backgroundColor = 'white'
+}
+function fundoSenhaCinza() {
+		senha.style.backgroundColor = 'rgb(181, 181, 181)'
+}
